a3489b122760d6476260d6072f7791c94320ac22
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-10-23 19:48:35 +0700

    styling navigasi mode mobile

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -2,4 +2,6 @@ document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 });
 
-<button onclick="alert('Halo Dunia!')">Klik Saya</button>
+let nama = prompt("Siapa nama kamu?");
+alert("Halo, " + nama + "!");
+
