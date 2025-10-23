a3e2c671454756f9c6366ae647c1f1a1de05dab0
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-10-23 18:19:08 +0700

    Eksternal Script

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,6 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 });
+
+// script.js
+alert("Halo Dunia dari file eksternal!")
