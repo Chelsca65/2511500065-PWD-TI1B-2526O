6882a9b923c19af2c39d25c227b7820acdd7c559
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-10-24 17:14:35 +0700

    menambahkan fungsi validasi pada form kontak menggunakan alert

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,16 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 });
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  const nama = document.getElementById("txtNama").value.trim();
+  const email = document.getElementById("txtEmail").value.trim();
+  const pesan = document.getElementById("txtPesan").value.trim();
+
+  if (nama === "" || email === "" || pesan === "") {
+    alert("Semua kolom wajib diisi!");
+    e.preventDefault();
+  } else {
+    alert("Terima kasih, " + nama + "! Pesan Anda telah dikirim.");
+  }
+});
