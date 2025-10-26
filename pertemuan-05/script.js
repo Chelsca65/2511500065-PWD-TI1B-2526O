61deba5a9ebbd75b5bde26d7f5257151b8481920
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-10-26 12:31:17 +0700

    tambah layout responsif untuk textarea dan counter pesan

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,6 +1,7 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
   const nav = document.querySelector("nav");
   nav.classList.toggle("active");
+
   if (nav.classList.contains("active")) {
     this.textContent = "\u2716";
   } else {
