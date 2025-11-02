77ecbe0315bc2dc4a7b0b7f9ee27f653019a3232
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-11-02 20:05:54 +0700

    Tambah style untuk section IPK

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -1,3 +1,163 @@
 <?php
     echo "Halo Dunia!";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Belajar PHP Dasar</title>
+</head>
+<body>
+    <h1><?php echo "Halo, Dunia PHP!"; ?></h1>
+    <h2><?php echo "Selamat datang di PHP, Chelsea!"; ?></h2>
+</body>
+</html>
+
+<?php
+$nama = "Chelsea";
+$umur = 18;
+$tinggi = 150;
+$aktif = true;
+
+echo "Nama: $nama <br>";
+echo "Umur: $umur tahun <br>";
+echo "Tinggi: $tinggi centimeter <br>";
+echo "Status aktif: " . ($aktif ? "Ya" : "Tidak") . "<br>";
+var_dump($nama);
+var_dump($umur);
+var_dump($tinggi);
+var_dump($aktif);
+?>
+
+<?php
+$nama = "Chelsea Clarisa";
+$umur = 18;
+$tinggi = 150;
+$aktif = true;
+$hobi = ["Menonton", "Musik"];
+$mahasiswa = (object)[
+"nim" => "2511500065",
+"nama" => "Chelsea Clarisa",
+"prodi" => "Teknik Informatika"
+];
+$nilai_akhir = NULL;
+echo "<h2>Demo Tipe Data PHP</h2>";
+
+echo "<pre>";
+echo "String:\n";
+var_dump($nama);
+
+echo "\nInteger:\n";
+var_dump($umur);
+
+echo "\nFloat:\n";
+var_dump($tinggi);
+
+echo "\nBoolean:\n";
+var_dump($aktif);
+
+echo "\nArray:\n";
+var_dump($hobi);
+?>
+
+<?php
+define("KAMPUS", "ISB Atma Luhur");
+const ANGKATAN = 2025;
+
+echo "Kampus: " . KAMPUS . "<br>";
+echo "Angkatan: " . ANGKATAN . "<br>";
+
+define("MATKUL", "Pemograman Web Dasar");
+define("DOSEN_PENGAMPU", "Pak Yohanes");
+
+echo "Mata kuliah: " . MATKUL . "<br>";
+echo "Dosen Pengampu: " . DOSEN_PENGAMPU;
+?>
+
+<?php
+// Ini komentar 1 baris
+# Ini juga komentar 1 baris
+/*
+Ini komentar
+lebih dari satu baris
+*/
+?>
+
+<?php
+$a = 10;
+$b = 3;
+echo $a + $b;
+echo $a % $b;
+?>
+
+<?php
+$a = 100;
+$b = "100";
+$c = 0;
+$d = false;
+echo "<h3>Perbandingan == dan ===</h3>";
+echo "\$a == \$b : "; var_dump($a == $b);
+echo "\$a === \$b : "; var_dump($a === $b);
+echo "\$c == \$d : "; var_dump($c == $d);
+echo "\$c === \$d : "; var_dump($c === $d);
+?>
+
+<?php
+$nilai = 80;
+if ($nilai >= 90) {
+echo "A";
+} elseif ($nilai >= 80) {
+echo "B";
+} else {
+echo "C";
+}
+?>
+
+<?php
+$hari = "Senin";
+switch ($hari) {
+case "Senin": echo "Awal Minggu!"; break;
+case "Jumat": echo "Hampir weekend!"; break;
+default: echo "Hari biasa.";
+}
+?>
+
+<?php
+$hobi = ["Menonton", "Musik"];
+foreach ($hobi as $item) {
+echo "Hobi: $item <br>";
+}
+?>
+
+<?php
+$hobi = ["Menonton", "Memasak", "Musik", "Membaca", "Traveling"];
+echo "<h3>Daftar Hobi Saya:</h3>";
+for ($i = 0; $i < count($hobi); $i++) {
+echo ($i + 1) . ". " . $hobi[$i] . "<br>";
+}
+echo "<hr>";
+echo "<h4>Hasil print_r():</h4>";
+echo "<pre>";
+print_r($hobi);
+echo "</pre>";
+echo "<h4>Hasil var_dump():</h4>";
+echo "<pre>";
+var_dump($hobi);
+echo "</pre>";
+?>
+
+<?php
+for ($i=1; $i<=5; $i++) {
+echo "Perulangan ke-$i <br>";
+}
+?>
+
+<?php
+$i = 1;
+do {
+echo "Iterasi ke-$i<br>";
+$i++;
+} while ($i <= 5);
+?>
+
+
--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -19,6 +19,7 @@
                 <li><a href="#home">Beranda</a></li>
                 <li><a href="#about">Tentang</a></li>
                 <li><a href="#contact">Kontak</a></li>
+                <li><a href="#ipk">nilai</a></li>
             </ul>
         </nav>
     </header>
@@ -26,20 +27,35 @@
         <section id="home">
             <h2>Selamat Datang</h2>
             <p>Ini contoh paragraf HTML.</p>
+            <?php
+            echo "Halo Dunia!";
+            ?>
         </section>
+        
         <section id="about">
+            <?php
+                $nim = "2511500065";
+                $namalengkap = "Chelsea Clarisa &#129321;";
+                $tempatlahir = "Pangkalpinang";
+                $tanggallahir = "12 Mei 2007";
+                $hobi = "Belajar, menonton film, mendengarkan musik &#127925;";
+                $pasangan = "Matthew Ignatius Susanto &hearts;";
+                $pekerjaan = "Mahasiswi di ISB Atma Luhur &copy; 2025";
+                $namaorangtua = "Liu Yin Siang dan Chew Kim Thui";
+                $namakakak = "Fitri Natalia dan Evans Leonardo";
+                $namaadik = "Jennifer Gracia";
+            ?>
             <h2>Tentang Saya</h2>
-            <p><strong>NIM:</strong> 2511500065</p>
-            <p><strong>Nama Lengkap:</strong> Chelsea Clarisa &#129321;</p>
-            <p><strong>Tempat Lahir:</strong> Pangkalpinang</p>
-            <p><strong>Tanggal Lahir:</strong> 12 Mei 2007</p>
-            <p><strong>Hobi:</strong> Belajar, menonton film, dan mendengarkan musik &#127925;</p>
-            <p><strong>Pasangan:</strong> Matthew Ignatius Susanto &hearts;</p>
-            <p><strong>Pekerjaan:</strong> Mahasiswi di ISB Atma Luhur &copy; 2025</p>
-            <p><strong>Nama Orang Tua:</strong> Bapak Chew Kim Thui dan Ibu Liu Yin Siang</p>
-            <p><strong>Nama Kakak Perempuan:</strong> Fitri Natalia</p>
-            <p><strong>Nama Kakak Laki-Laki:</strong> Evans Leonardo</p>
-            <p><strong>Nama Adik Perempuan:</strong> Jennifer Gracia</p>
+            <p><strong>NIM:</strong> <?php echo $nim ?></p>
+            <p><strong>Nama Lengkap:</strong> <?php echo $namalengkap ?></p>
+            <p><strong>Tempat Lahir:</strong> <?php echo $tempatlahir ?></p>
+            <p><strong>Tanggal Lahir:</strong> <?php echo $tanggallahir ?></p>
+            <p><strong>Hobi:</strong> <?php echo $hobi ?></p>
+            <p><strong>Pasangan:</strong> <?php echo $pasangan ?></p>
+            <p><strong>Pekerjaan:</strong> <?php echo $pekerjaan ?></p>
+            <p><strong>Nama Orang Tua:</strong> <?php echo $namaorangtua ?></p>
+            <p><strong>Nama Kakak:</strong> <?php echo $namakakak ?></p>
+            <p><strong>Nama Adik:</strong> <?php echo $namaadik ?></p>
         </section>
         <section id="contact">
             <h2>Kontak Kami</h2>
@@ -53,7 +69,7 @@
                 </label>
 
                 <label for="txtpesan"><span>Pesan Anda:</span>
-                    <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+                    <textarea id="txtpesan" name="txtpesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
                      <small id="charCount">0/200 karakter</small> 
                 </label>
 
@@ -61,6 +77,109 @@
                 <button type="reset">Batal</button>
             </form>
         </section>
+
+        <section id="ipk">
+            <?php
+                $namaMatkul1 = "Algoritma dan Struktur Data";
+                $namaMatkul2 = "Agama";
+                $namaMatkul3 = "Konsep Basis Data";
+                $namaMatkul4 = "Kalkulus";
+                $namaMatkul5 = "Pemrograman Web Dasar";
+
+                $sksMatkul1 = 4;
+                $sksMatkul2 = 2;
+                $sksMatkul3 = 3;
+                $sksMatkul4 = 3;
+                $sksMatkul5 = 3;
+
+                $nilaiHadir1 = 90; $nilaiTugas1 = 60; $nilaiUTS1 = 80; $nilaiUAS1 = 70;
+                $nilaiHadir2 = 70; $nilaiTugas2 = 50; $nilaiUTS2 = 60; $nilaiUAS2 = 80;
+                $nilaiHadir3 = 80; $nilaiTugas3 = 80; $nilaiUTS3 = 80; $nilaiUAS3 = 90;
+                $nilaiHadir4 = 90; $nilaiTugas4 = 75; $nilaiUTS4 = 80; $nilaiUAS4 = 85;
+                $nilaiHadir5 = 69; $nilaiTugas5 = 80; $nilaiUTS5 = 90; $nilaiUAS5 = 100;
+
+                function angkaMutu($grade){
+                    switch ($grade) {
+                        case "A": $angkaMutu = 4.00; break;
+                        case "A-": $angkaMutu = 3.70; break;
+                        case "B+": $angkaMutu = 3.30; break;
+                        case "B": $angkaMutu = 3.00; break;
+                        case "C+": $angkaMutu = 2.70; break;
+                        case "C": $angkaMutu = 2.00; break;
+                        case "D+": $angkaMutu = 1.70; break;
+                        case "D": $angkaMutu = 1.00; break;
+                        default: $angkaMutu = 0.00; break;
+                }
+                return $angkaMutu;
+            }
+                
+                 
+                $totalBobot = 0;
+                $totalSks = 0;
+
+                for ($i = 1; $i <= 5; $i++) {
+                    ${"nilaiAkhir$i"} = round((0.1 * ${"nilaiHadir$i"}) + (0.2 * ${"nilaiTugas$i"}) + (0.3 * ${"nilaiUTS$i"}) + (0.4 * ${"nilaiUAS$i"}));
+
+                if (${"nilaiHadir$i"} < 70) {
+                    ${"grade$i"} = "E";
+                } elseif (${"nilaiAkhir$i"} >= 91) {
+                    ${"grade$i"} = "A";
+                } elseif (${"nilaiAkhir$i"} >= 81) {
+                    ${"grade$i"} = "A-";
+                } elseif (${"nilaiAkhir$i"} >= 76) {
+                    ${"grade$i"} = "B+";
+                } elseif (${"nilaiAkhir$i"} >= 71) {
+                    ${"grade$i"} = "B";
+                } elseif (${"nilaiAkhir$i"} >= 66) {
+                    ${"grade$i"} = "C+";
+                } elseif (${"nilaiAkhir$i"} >= 61) {
+                    ${"grade$i"} = "C";
+                } elseif (${"nilaiAkhir$i"} >= 36) {
+                    ${"grade$i"} = "D";
+                } else {
+                    ${"grade$i"} = "E";
+                } 
+
+                if (${"grade$i"} == "D" || ${"grade$i"} == "E") {
+                    ${"status$i"} = "Gagal";
+                } else {
+                    ${"status$i"} = "Lulus";
+                }
+
+                $angkaMutu = angkaMutu(${"grade$i"});
+                ${"bobot$i"} = $angkaMutu * ${"sksMatkul$i"};
+                $totalBobot += ${"bobot$i"};
+                $totalSks += ${"sksMatkul$i"};
+                $ipk = $totalBobot / $totalSks; 
+
+            }
+            ?>
+
+            <h2>Nilai Saya</h2>
+            <?php 
+            for ($i = 1; $i <= 5; $i++) { ?>
+            <p><strong>Nama Matakuliah ke-<?php echo $i; ?>:</strong> <?php echo ${"namaMatkul$i"}; ?></p>
+            <p><strong>SKS:</strong> <?php echo ${"sksMatkul$i"};?></p>
+            <p><strong>Kehadiran:</strong> <?php echo ${"nilaiHadir$i"}; ?></p>
+            <p><strong>Tugas:</strong> <?php echo ${"nilaiTugas$i"}; ?></p>
+            <p><strong>UTS:</strong> <?php echo ${"nilaiUTS$i"}; ?></p>
+            <P><strong>UAS:</strong> <?php echo ${"nilaiUAS$i"}; ?></p>
+            <p><strong>Nilai Akhir:</strong> <?php echo (${"nilaiAkhir$i"}); ?></p>
+            <p><strong>Grade:</strong> <?php echo ${"grade$i"}; ?></p>
+            <p><strong>Angka Mutu:</strong> <?php echo number_format(angkaMutu(${"grade$i"}), 2); ?></p>
+            <p><strong>Bobot:</strong> <?php echo number_format (${"bobot$i"}, 2); ?></p>
+            <p><strong>Status:</strong> <?php echo ${"status$i"}; ?></p>
+
+            <?php 
+            echo "<p><strong>Total Bobot: </strong>" . number_format($totalBobot, 2) . "</p>";
+            echo "<p><strong>Total SKS: </strong>" . $totalSks . "</p>";
+            echo "<p><strong>IPK: </strong>" . number_format($ipk, 2). "</p>";
+            } 
+        ?>
+        
+
+        </section>
+
     </main>
     <footer>
         <p>&copy; 2025 Chelsea Clarisa [2511500065]</p>
