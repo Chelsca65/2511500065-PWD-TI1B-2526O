a9c5af4eb1142bb9c9d1a41c5772031326d16972
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-11-09 16:23:23 +0700

    isset

--- pertemuan-07/get.php
+++ pertemuan-07/get.php
@@ -1,5 +1,9 @@
 <?php
   session_start();
+  $sesname = "";
+  if (isset($_SESSION["nama"])):
+    $sesname = $_SESSION["nama"];
+  endif;
   $sesname = $_SESSION["nama"];
   $sesemail = $_SESSION["email"];
   $sespesan = $_SESSION["pesan"];
