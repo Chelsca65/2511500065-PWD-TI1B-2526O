47a802caf7306eea07d04238429667fd26f275ae
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2025-11-27 11:58:46 +0700

    test

--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -7,16 +7,20 @@ $_SESSION["sesnama"] = $sesnama;
 $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
 
-$txtNim = $_POST["txtNim"];
-$txtNmLengkap = $_POST["txtNmLengkap"];
-$txtT4Lhr = $_POST["txtT4Lhr"];
-$txtTglLhr = $_POST["txtTglLhr"];
-$txtHobi = $_POST["txtHobi"];
-$txtPasangan = $_POST["txtPasangan"];
-$txtKerja = $_POST["txtKerja"];
-$txtNmOrtu = $_POST["txtNmOrtu"];
-$txtNmKakak = $_POST["txtNmKakak"];
-$txtNmAdik = $_POST["txtNmAdik"];
+$arrBiodata = [
+"nim" => $_POST["txtNim"] ?? "",
+"nama"=> $_POST["txtNmLengkap"] ?? "",
+"tempat" => $_POST["txtT4Lhr"] ?? "",
+"tanggal" => $_POST["txtTglLhr"] ?? "",
+"hobi"=> $_POST["txtHobi"] ?? "",
+"pasangan" => $_POST["txtPasangan"] ?? "",
+"pekerjaan"=> $_POST["txtKerja"] ?? "",
+"ortu"=> $_POST["txtNmOrtu"] ?? "",
+"kakak"=> $_POST["txtNmKakak"] ?? "",
+"adik"=> $_POST["txtNmAdik"] ?? ""
+];
+
+foreach ($dataBiodata as $k => $a)
 
 $_SESSION["txtNim"] = $txtNim;
 $_SESSION["txtNmLengkap"] = $txtNmLengkap;
