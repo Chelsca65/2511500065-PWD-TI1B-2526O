302f805079515dcafc1e50a455c06efd6995afe9
Chelsea Clarisa <2511500065@mahasiswa.atmaluhur.ac.id>
2026-01-14 06:49:05 +0700

    buat file read.php untuk menampilkan data mahasiswa

--- pertemuan-15/README.md
+++ pertemuan-15/README.md
@@ -6,4 +6,6 @@ Hari ini, Rabu 14.Jan.2026 saya belajar:<br>
 <ol>
     <li>buat tabel biodata_mahasiswa di phpmyadmin</li>
     <li>buat file koneksi.php dan tambahkan kode yang dipakai untuk koneksi ke database</li>
+    <li>buat tabel biodata_mahasiswa di phpmyadmin</li>
+    <li>buat file read.php untuk menampilkan data mahasiswa</li>
 </ol>
---
+++ pertemuan-15/read.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+require 'koneksi.php';
+require 'fungsi.php';
+
+$sql = "SELECT * FROM biodata_mahasiswa ORDER BY id DESC";
+$q   = mysqli_query($conn, $sql);
+if (!$q) {
+  die("Query error: " . mysqli_error($conn));
+}
+?>
+
+<?php
+$flash_sukses_biodata = $_SESSION['flash_sukses_biodata'] ?? ''; #jika query sukses
+$flash_error_biodata = $_SESSION['flash_error_biodata'] ?? ''; #jika ada error
+#bersihkan session ini
+unset($_SESSION['flash_sukses_biodata'], $_SESSION['flash_error_biodata']);
+?>
+
+<?php if (!empty($flash_sukses_biodata)): ?>
+  <div style="padding:10px; margin-bottom:10px; 
+          background:#d4edda; color:#155724; border-radius:6px;">
+    <?= $flash_sukses_biodata; ?>
+  </div>
+<?php endif; ?>
+
+<?php if (!empty($flash_error_biodata)): ?>
+  <div style="padding:10px; margin-bottom:10px; 
+          background:#f8d7da; color:#721c24; border-radius:6px;">
+    <?= $flash_error_biodata; ?>
+  </div>
+<?php endif; ?>
+
+<table border="1" cellpadding="8" cellspacing="0">
+  <tr>
+    <th>No</th>
+    <th>aksi</th>
+    <th>ID</th>
+    <th>NIM</th>
+    <th>Nama</th>
+    <th>Tempat Lahir</th>
+    <th>Tanggal Lahir</th>
+    <th>Hobi</th>
+    <th>Pasangan</th>
+    <th>Pekerjaan</th>
+    <th>Orang Tua</th>
+    <th>Kakak</th>
+    <th>Adik</th>
+  </tr>
+
+  <?php $i = 1; ?>
+  <?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <tr>
+      <td><?= $i++ ?></td>
+      <td><?= $row['id']; ?></td>
+      <td>
+        <a href="edit.php?cid=<?= (int)$row['id']; ?>">Edit</a>
+        <a onclick="return confirm('Hapus <?= htmlspecialchars($row['nama_lengkap']); ?> ?')" href="proses_delete.php?cid=<?= (int)$row['id']; ?>">Delete</a>
+      </td>
+      <td><?= htmlspecialchars($row['nim']); ?></td>
+      <td><?= htmlspecialchars($row['nama_lengkap']); ?></td>
+      <td><?= htmlspecialchars($row['tempat_lahir']); ?></td>
+      <td><?= htmlspecialchars($row['tanggal_lahir']); ?></td>
+      <td><?= htmlspecialchars($row['hobi']); ?></td>
+      <td><?= htmlspecialchars($row['pasangan']); ?></td>
+      <td><?= htmlspecialchars($row['pekerjaan']); ?></td>
+      <td><?= htmlspecialchars($row['nama_orang_tua']); ?></td>
+      <td><?= htmlspecialchars($row['nama_kakak']); ?></td>
+      <td><?= htmlspecialchars($row['nama_adik']); ?></td>
+    </tr>
+  <?php endwhile; ?>
+</table>
